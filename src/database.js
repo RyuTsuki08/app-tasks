2ba76c2e2d49855207211a833d9048fac63b5328
TomasA <79181564+RyuTsuki08@users.noreply.github.com>
2022-08-08 17:43:46 -0400

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const URI = 'mongodb+srv://RyuTsuki08:tomasP2112.@ryutsuki088.wkhqi.mongodb.net/RyuTsuki088?retryWrites=true&w=majority'
+const URI = 'mongodb+srv://RyuTsuki08:<passwordcluster>@ryutsuki088.wkhqi.mongodb.net/RyuTsuki088?retryWrites=true&w=majority'
 
 mongoose.connect(URI)
     .then(db => console.log('connected to database'))
